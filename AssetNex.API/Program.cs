c5fad0c11e949a6e587fd85adc11e409ffe6fdc6
RuheenKaur <rkaur2_be21@thapar.edu>
2026-03-25 12:16:34 +0530

    enable swagger in production

--- AssetNex.API/Program.cs
+++ AssetNex.API/Program.cs
@@ -169,9 +169,13 @@ using (var scope = app.Services.CreateScope())
 
 Console.WriteLine("JWT KEY -> " + builder.Configuration["JwtSettings:Key"]);
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "AssetNexIT API v1");
+    c.RoutePrefix = "swagger";
+});
 app.UseStaticFiles(); 
 app.UseCors("AllowAngular");
 app.UseHttpsRedirection();
