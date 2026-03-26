ef98a15aaf71cd1c4f9692c9be2d0cedb8c978ab
RuheenKaur <rkaur2_be21@thapar.edu>
2026-03-26 10:21:01 +0530

    Azure fully working - JWT auth, migrations, pagination live

---
+++ AssetNex.API/Migrations/AppDb/20260326044536_FreshAzureStart.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetNex.API.Migrations.AppDb
+{
+    /// <inheritdoc />
+    public partial class FreshAzureStart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Asset_Software",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Version = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusId = table.Column<int>(type: "int", nullable: false),
+                    AssetId = table.Column<int>(type: "int", nullable: false),
+                    License_Key = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InstalledAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Asset_Software", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssetMaster",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StatusId = table.Column<int>(type: "int", nullable: false),
+                    AssetTag = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AssetType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SerialNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RAM_GB = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Storage_GB = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PurchaseCost = table.Column<int>(type: "int", nullable: false),
+                    WarrantyDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PurchaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssetMaster", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StatusMaster",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StatusName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusCategory = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusCode = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusMaster", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TicketComments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TicketId = table.Column<int>(type: "int", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommentedByUserId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketComments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contact = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssetAssignments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AssetId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    ReturnedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    AssetAssigned = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AssignedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssetAssignments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssetAssignments_AssetMaster_AssetId",
+                        column: x => x.AssetId,
+                        principalTable: "AssetMaster",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssetHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AssetId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    StatusId = table.Column<int>(type: "int", nullable: false),
+                    EventType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EventDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReferenceTicketId = table.Column<int>(type: "int", nullable: false),
+                    AssignedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReturnedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Remarks = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PerformedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Vendor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CostIncurred = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssetHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssetHistory_AssetMaster_AssetId",
+                        column: x => x.AssetId,
+                        principalTable: "AssetMaster",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AssetHistory_StatusMaster_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "StatusMaster",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AssetHistory_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AssetRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AssetId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    RequestedAssetType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusId = table.Column<int>(type: "int", nullable: false),
+                    RequestedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssetRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssetRequests_AssetMaster_AssetId",
+                        column: x => x.AssetId,
+                        principalTable: "AssetMaster",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AssetRequests_StatusMaster_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "StatusMaster",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AssetRequests_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SupportTickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<int>(type: "int", nullable: false),
+                    AssetId = table.Column<int>(type: "int", nullable: false),
+                    IssueCategory = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IssueDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Priority = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ResolutionNotes = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SupportTickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SupportTickets_AssetMaster_AssetId",
+                        column: x => x.AssetId,
+                        principalTable: "AssetMaster",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SupportTickets_StatusMaster_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "StatusMaster",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SupportTickets_Users_CreatedBy",
+                        column: x => x.CreatedBy,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetAssignments_AssetId",
+                table: "AssetAssignments",
+                column: "AssetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetHistory_AssetId",
+                table: "AssetHistory",
+                column: "AssetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetHistory_StatusId",
+                table: "AssetHistory",
+                column: "StatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetHistory_UserId",
+                table: "AssetHistory",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetRequests_AssetId",
+                table: "AssetRequests",
+                column: "AssetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetRequests_StatusId",
+                table: "AssetRequests",
+                column: "StatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssetRequests_UserId",
+                table: "AssetRequests",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SupportTickets_AssetId",
+                table: "SupportTickets",
+                column: "AssetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SupportTickets_CreatedBy",
+                table: "SupportTickets",
+                column: "CreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SupportTickets_StatusId",
+                table: "SupportTickets",
+                column: "StatusId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Asset_Software");
+
+            migrationBuilder.DropTable(
+                name: "AssetAssignments");
+
+            migrationBuilder.DropTable(
+                name: "AssetHistory");
+
+            migrationBuilder.DropTable(
+                name: "AssetRequests");
+
+            migrationBuilder.DropTable(
+                name: "SupportTickets");
+
+            migrationBuilder.DropTable(
+                name: "TicketComments");
+
+            migrationBuilder.DropTable(
+                name: "AssetMaster");
+
+            migrationBuilder.DropTable(
+                name: "StatusMaster");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
